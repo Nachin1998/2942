1f94a4df4205c604df8a6f74c022e54c8b5f67ca
Ignacio <Ignacio.f.l@hotmail.com>
2020-07-11 22:06:38 -0300

    Made main menu and game transition, ScenesManager and pause script. Need to finish main game and add chimi.

---
+++ Assets/Scripts/Bullet/Bullet.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float movementSpeed;
+
+    [HideInInspector] public SpriteRenderer sr;
+    [HideInInspector] public Rigidbody2D rb;
+    [HideInInspector] public BoxCollider2D bc;
+    [HideInInspector] public ParticleSystem ps;
+
+    [HideInInspector] public Vector2 speed;
+
+    public float timeUntillDestroyed;
+
+    public void GetComponents()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+        bc = GetComponent<BoxCollider2D>();
+        ps = GetComponentInChildren<ParticleSystem>();
+
+        speed = new Vector2(0, movementSpeed);
+    }
+
+    public void MoveBullet()
+    {
+        rb.velocity = speed * Time.deltaTime;
+    }
+
+    public void DestroyBulletAfter(float time)
+    {
+        timeUntillDestroyed += Time.deltaTime;
+
+        if(timeUntillDestroyed > time)
+        {
+            StartCoroutine(DestroyBullet());
+        }
+    }
+
+    public IEnumerator DestroyBullet()
+    {
+        sr.color = Color.clear;
+        Destroy(bc);
+        ps.Stop();
+
+        yield return new WaitForSeconds(0.3f);
+        Destroy(ps);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/ScenesManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesManager : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
